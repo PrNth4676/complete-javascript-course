f50d3590435705ffb19cf83389de99f1483dc399
Pratik Nath <48478737+PrNth4676@users.noreply.github.com>
2025-05-03 07:28:31 +0530

    JS Day 01

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -1,19 +1,18 @@
-/*
 ////////////////////////////////////
 // Linking a JavaScript File
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
-////////////////////////////////////
-// Values and Variables
+// ////////////////////////////////////
+// // Values and Variables
 console.log("Jonas");
 console.log(23);
 
 let firstName = "Matilda";
 
 console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
 // Variable name conventions
 let jonas_matilda = "JM";
@@ -35,22 +34,23 @@ console.log(myFirstJob);
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
-// console.log(typeof true);
+// typeof shows the data type
+console.log(typeof true);
 console.log(typeof javascriptIsFun);
-// console.log(typeof 23);
-// console.log(typeof 'Jonas');
+console.log(typeof 23);
+console.log(typeof 'Jonas');
 
-javascriptIsFun = 'YES!';
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
 
-let year;
+let year; // Variable is not defined
 console.log(year);
 console.log(typeof year);
 
 year = 1991;
-console.log(typeof year);
+console.log(typeof year); //After defining with a value, the type is now number
 
-console.log(typeof null);
+console.log(typeof null); // In JS, null is defined as an object 
 
 ////////////////////////////////////
 // let, const and var
@@ -58,8 +58,8 @@ let age = 30;
 age = 31;
 
 const birthYear = 1991;
-// birthYear = 1990;
-// const job;
+// birthYear = 1990; // const once assigned cannot be changed
+// const job; // const cannot be left undefined
 
 var job = 'programmer';
 job = 'teacher'
@@ -67,53 +67,53 @@ job = 'teacher'
 lastName = 'Schmedtmann';
 console.log(lastName);
 
-////////////////////////////////////
-// Basic Operators
-// Math operators
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
+// ////////////////////////////////////
+// // Basic Operators
+// // Math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
 
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-const firstName = 'Jonas';
-const lastName = 'Schmedtmann';
-console.log(firstName + ' ' + lastName);
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
 
-// Assignment operators
-let x = 10 + 5; // 15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; // x = x + 1
-x--;
-x--;
-console.log(x);
+// // Assignment operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--;
+// x--;
+// console.log(x);
 
-// Comparison operators
-console.log(ageJonas > ageSarah); // >, <, >=, <=
-console.log(ageSarah >= 18);
+// // Comparison operators
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
 
-const isFullAge = ageSarah >= 18;
+// const isFullAge = ageSarah >= 18;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
 
-////////////////////////////////////
-// Operator Precedence
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
+// ////////////////////////////////////
+// // Operator Precedence
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
 
-let x, y;
-x = y = 25 - 10 - 5; // x = y = 10, x = 10
-console.log(x, y);
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10, x = 10
+// console.log(x, y);
 
-const averageAge = (ageJonas + ageSarah) / 2;
-console.log(ageJonas, ageSarah, averageAge);
-*/
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
 
 ////////////////////////////////////
 // Coding Challenge #1
